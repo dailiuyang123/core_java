2979e2d9b01f483a24fb9af61df9e0a679a01ba8
json <17600608814@163.com>
2018-11-01 09:45:10 +0800

    测试 by company

--- src/test/java/com/daily/coreJava/TestGit.java
+++ src/test/java/com/daily/coreJava/TestGit.java
@@ -4,4 +4,7 @@ package com.daily.coreJava;
  * Created by Azir on 2018/10/31.
  */
 public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("测试 SSH 登录");
+    }
 }
